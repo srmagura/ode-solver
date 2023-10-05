d8aa85a9d670542a7590b3fc7d2e20e695d5dac8
Sam Magura <srmagura@gmail.com>
2023-10-05 10:26:45 -0400

    [wip] [skip ci]

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+//import "fmt"
+import "math"
+
+var tInitial = 0
+var tFinal = 10
+
+var yInitial = 1
+
+func evalYExpected(t float64) float64 {
+
+}
+
+func evalF(t float64) float64 {
+	return math.Sin(t)
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+
 }
